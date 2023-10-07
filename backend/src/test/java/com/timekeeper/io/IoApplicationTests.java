2facc43211c96df5456c1b03b8c1ba8004ee15c7
0815pascal <pascal.witzig@gmail.com>
2023-10-07 20:18:01 +0200

    Basic Boilerplate Setup

---
+++ backend/src/test/java/com/timekeeper/io/IoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timekeeper.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
